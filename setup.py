cf422d5698ba69bebe5552724a34d2d29b4ea785
John McFarlane <john.mcfarlane@rockfloat.com>
2010-11-30 23:11:04 -0800

    Everything seems to be working again

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     author_email = EMAIL,
     entry_points = """
         [console_scripts]
-        pubdiff = pubdiff.client:main
+        pubdiff = client:main
     """,
     name = NAME,
     packages = find_packages(),
