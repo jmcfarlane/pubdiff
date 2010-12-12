41d269c725735c5238271813c5fc305001794fc4
John McFarlane <john.mcfarlane@rockfloat.com>
2010-12-12 01:48:44 -0800

    Update build process, now uploading to pipy

--- setup.py
+++ setup.py
@@ -1,22 +1,42 @@
-from setuptools import setup, find_packages
+# Python imports
+import os
+import sys
+
+# Third party imports
+from setuptools import setup
+
+# Project imports
+from pubdiff import client
 
 # Attributes
+SRC = os.path.dirname(sys.argv[0])
 AUTHOR = 'John McFarlane'
+DESCRIPTION = open(os.path.join(SRC, 'README')).readlines()[0].strip()
 EMAIL = 'john.mcfarlane@rockfloat.com'
 NAME = 'Pubdiff'
 URL = 'http://www.pubdiff.com'
-
-from pubdiff import client
+CLASSIFIERS = """
+Development Status :: 2 - Pre-Alpha
+Intended Audience :: Developers
+License :: OSI Approved :: MIT License
+Operating System :: OS Independent
+Programming Language :: Python
+Topic :: Internet :: WWW/HTTP
+Topic :: Software Development :: Version Control
+"""
 
 setup(
     author = AUTHOR,
     author_email = EMAIL,
+    classifiers = [c for c in CLASSIFIERS.split('\n') if c],
+    description = DESCRIPTION,
+    download_url = '%s/downloads/Pubdiff-%s.tar.gz' % (URL, client.VERSION),
     entry_points = """
         [console_scripts]
         pubdiff = pubdiff.client:main
     """,
     name = NAME,
-    packages = find_packages(),
+    packages = ['pubdiff'],
     url = URL,
     version = client.VERSION,
     zip_safe = True,
