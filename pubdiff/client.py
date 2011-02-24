f3ea9310fd9664e173cad48b161ec198cc171d9b
John McFarlane <john.mcfarlane@rockfloat.com>
2011-02-24 00:23:14 -0800

    Begin work on 0.0.8

--- pubdiff/client.py
+++ pubdiff/client.py
@@ -17,7 +17,7 @@ BEFORE = 'before'
 CONTENTS = 'contents'
 NAME = 'name'
 URL = config.get('core', 'url') + '/api/upload'
-VERSION = '0.0.7'
+VERSION = '0.0.8'
 
 class SourceFile(dict):
     def __init__(self, name=None, contents=None):
