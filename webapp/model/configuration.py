6fb70d578a94e9e109df45ebad07715091ddce57
John McFarlane <john.mcfarlane@rockfloat.com>
2011-03-08 02:33:52 -0800

    Remove code now provided by upstream Chula

--- webapp/model/configuration.py
+++ webapp/model/configuration.py
@@ -9,13 +9,12 @@ from model import review
 
 # App configuration
 app = config.Config()
+app.local.root = os.path.join(os.path.dirname(__file__), '..')
 app.classpath = 'controller'
-app.construction_controller = 'error'
 app.construction_trigger = '/tmp/pubdiff/site.stop'
+app.htdocs = os.path.join(app.local.root, 'www')
 app.debug = 'debug' in os.environ
-app.error_controller = 'error'
 app.session = False
-app.local.root = os.path.join(os.getcwd(), 'webapp')
 
 # App url routes
 app.mapper = (
